5944cecd12338e2f950ac8cd749f3a473847f054
Avi Greenbury <ialoneambest@gmail.com>
2026-06-22 10:50:21 +0100

    longhorn: set longhorn as default storageclass, require only 1 replica

--- longhorn/install.sh
+++ longhorn/install.sh
@@ -6,5 +6,9 @@ helm install longhorn longhorn/longhorn \
 	--namespace longhorn-system \
 	--create-namespace \
 	--version 1.8.1 \
-	--set defaultSettings.defaultDataPath=/var/lib/k8s/longhorn
+	--set defaultSettings.defaultDataPath=/var/lib/k8s/longhorn \
+	--set defaultSettings.defaultReplicaCount=1
 
+# k3s sets local-path to the default storageclass, longhorn _adds_ itself as another
+# default, so here we unmark local-path as a default:
+kubectl patch storageclass local-path	-p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
